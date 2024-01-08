1eee878f0568ac1e1566e6395c20a079df9b00c3
amrapali gangurde <gangurdeamrapali2001@gmail.com>
2024-01-08 21:18:32 +0530

    first commit

---
+++ myweb.js
@@ -0,0 +1,9 @@
+
+//  for order form 
+function showOrderForm(dessertName) {
+
+    // Display
+    document.getElementById('order-form').style.display = 'block';
+  
+    document.getElementById('dessert-name').textContent = dessertName;
+}
